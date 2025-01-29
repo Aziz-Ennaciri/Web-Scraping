2ab2195bb42b84781014749326c8716103f54fc8
Aziz-Ennaciri <azizennaciri0@gmail.com>
2025-01-29 17:04:55 +0100

    Update

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -4,15 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const table1 = document.getElementById("table");
     const charts1 = document.getElementById("charts");
 
+    function setActiveLink(activeLink) {
+        document.querySelectorAll(".nav-link").forEach(link => link.classList.remove("active"));
+        activeLink.classList.add("active");
+    }
+
     tableView.addEventListener("click", (e) => {
         e.preventDefault();
         console.log("Table clicked");
         table1.style.display = "block";
         charts1.style.display = "none";
+        setActiveLink(tableView);
     });
+
     chartsView.addEventListener("click", (e) => {
         e.preventDefault();
         console.log("Charts clicked");
         table1.style.display = "none";
         charts1.style.display = "block";
-    });});
+        setActiveLink(chartsView);
+    });
+});
